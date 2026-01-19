fb490120fe638921b3b64517f476db0a2ec4abda
stancucatinca <168513402+stancucatinca@users.noreply.github.com>
2026-01-19 22:17:32 +0200

    Delete Ride sharing site_v_finala directory

--- Ride sharing site_v_finala/www/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Pornim sesiunea ca să știm pe cine deconectăm
-session_unset(); // Ștergem toate variabilele (Nume, ID, Rol)
-session_destroy(); // Distrugem sesiunea complet
-
-// Te trimitem înapoi pe prima pagină
-header("Location: index.php");
-exit();
-?>
